c285cbe51afee65e2d01ddcba60bc41f2a1a0bef
Ananya Gupta <anannya.gta@gmail.com>
2024-07-20 17:43:18 +0530

    "Added form handling and API call to SignUp page, and configured proxy in vite.config.js"

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,20 +2,61 @@ import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { HiOutlineUserAdd } from "react-icons/hi";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      // since loading is completed
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-bold text-[#172b4a] my-7">
         Sign Up
       </h1>
 
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Username"
           className="border p-3 rounded-lg"
           id="username"
+          onChange={handleChange}
         />
 
         <input
@@ -23,6 +64,7 @@ const SignUp = () => {
           placeholder="Email address"
           className="border p-3 rounded-lg"
           id="email"
+          onChange={handleChange}
         />
 
         <input
@@ -30,32 +72,40 @@ const SignUp = () => {
           placeholder="Password"
           className="border p-3 rounded-lg"
           id="password"
+          onChange={handleChange}
         />
 
-        <button className="bg-[#172b4a] text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80 flex justify-center items-center ">
-          <HiOutlineUserAdd class="w-5 h-5" />
-          <span class="ml-4">Sign Up</span>
+        <button
+          disabled={loading}
+          className="bg-[#172b4a] text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80 flex justify-center items-center "
+        >
+          <HiOutlineUserAdd className="w-5 h-5" />
+          <span className="ml-4">
+            {loading ? "Creating your account" : "Sign Up"}
+          </span>
         </button>
 
-        <div class="flex items-center space-x-4">
-          <hr class="w-full border border-gray-300" />
-          <div class="font-semibold text-gray-400">OR</div>
-          <hr class="w-full border border-gray-300" />
+        <div className="flex items-center space-x-4">
+          <hr className="w-full border border-gray-300" />
+          <div className="font-semibold text-gray-400">OR</div>
+          <hr className="w-full border border-gray-300" />
         </div>
 
-        <button class="bg-[#2c081011] border p-3 w-full rounded-lg mt-5 flex justify-center items-center  hover:opacity-90">
-          <FcGoogle class="w-6 h-6" />
-          <span class="ml-4 font-semibold">Continue with Google</span>
+        <button className="bg-[#2c081011] border p-3 w-full rounded-lg mt-5 flex justify-center items-center  hover:opacity-90">
+          <FcGoogle className="w-6 h-6" />
+          <span className="ml-4 font-semibold">Continue with Google</span>
         </button>
 
-        <p class="text-sm mt-6 text-center text-gray-800">
+        <p className="text-sm mt-6 text-center text-gray-800">
           Already have an account?
           <Link to={"/sign-in"}>
-            <span class="text-blue-600 font-semibold hover:underline ml-1">
+            <span className="text-blue-600 font-semibold hover:underline ml-1">
               Login here
             </span>
           </Link>
         </p>
+
+        {error && <p className="text-red-500 mt-5 text-center">{error}</p>}
       </form>
     </div>
   );
