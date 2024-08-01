30801c4ed5b350bf45bed8408ce1a6549f26d867
Ananya Gupta <anannya.gta@gmail.com>
2024-08-01 21:35:25 +0530

    asdfgh

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -6,6 +6,7 @@ const Profile = () => {
   const fileRef = useRef(null);
   const { currentUser } = useSelector((state) => state.user);
   return (
+    
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
