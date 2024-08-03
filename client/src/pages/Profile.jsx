b1739526fe42cc1b760d2bc3d99556953fa15f6a
Ananya Gupta <anannya.gta@gmail.com>
2024-08-03 15:00:33 +0530

    "Added file upload state and handler to Profile component"

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,16 +1,36 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useRef } from "react";
+import { useState, useEffect } from "react";
+import { getStorage } from "firebase/storage";
 
 const Profile = () => {
-  
   const fileRef = useRef(null);
   const { currentUser } = useSelector((state) => state.user);
+  const [file, setFile] = useState(undefined);
+  console.log(file);
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage();
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
-        <input type="file" ref={fileRef} hidden accept="image/*"/>
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+        />
         <img
           onClick={() => fileRef.current.click()}
           src={currentUser.avatar}
