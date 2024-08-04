37b878f8b26f45dbc41dc91378e788d126458511
Ananya Gupta <anannya.gta@gmail.com>
2024-08-04 12:55:47 +0530

    Remove empty line in test function of user.controller.js

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -4,7 +4,6 @@ import { errorHandler } from "../utils/error";
 import User from "../models/user.model";
 
 export const test = (req, res) => {
-  
   res.json({
     message: "API route is working",
   });
