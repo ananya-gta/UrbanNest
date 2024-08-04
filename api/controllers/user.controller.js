9cb3887de49aa0f473f9df3f7271fa35dfad675a
Ananya Gupta <anannya.gta@gmail.com>
2024-08-04 12:55:07 +0530

    "Updated user controller to hash passwords, added authentication middleware to user route, and modified verify token function"

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,7 @@
 // In these files we put the logic of req, res in the api routes
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error";
+import User from "../models/user.model";
 
 export const test = (req, res) => {
   res.json({
@@ -6,6 +9,30 @@ export const test = (req, res) => {
   });
 };
 
-export const updateUser = (req, res, next) => {
-  const { id, name, email } = req.body;
-}
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "you can update only your account"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { test, updateUser} from "../controllers/user.controller.js";
+import { test, updateUser } from "../controllers/user.controller.js";
+import { verifyToken } from "./../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
 
 //create update user wpi route
-router.post("/update/:id", updateUser);
+router.post("/update/:id", verifyToken, updateUser);
 
 // When you use export default in a module, you are specifying the default export for that module. This allows you to import the module in another file using any name you choose.
 export default router;
